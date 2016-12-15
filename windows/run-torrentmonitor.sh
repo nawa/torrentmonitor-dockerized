c01a023517bde4567508da11136859f9f7b44710
Siarhei Navatski <navatski@gmail.com>
2016-12-15 09:17:31 +0300

    Update vagrant script for new docker image

--- windows/run-torrentmonitor.sh
+++ windows/run-torrentmonitor.sh
@@ -3,5 +3,5 @@ isRunning=$(docker inspect -f {{.State.Running}} torrentmonitor)
 if [[ "$isRunning" == "false" ]]; then
 	docker start torrentmonitor
 else
-	docker run -d -p 8080:80 --name=torrentmonitor -v /vagrant/data/torrents:/usr/share/nginx/html/torrentmonitor/torrents -v /vagrant/data/db:/usr/share/nginx/html/torrentmonitor/db nawa/torrentmonitor
+	docker run -d -p 8080:80 --name=torrentmonitor -v /vagrant/data/torrents:/DATA/htdocs/torrents -v /vagrant/data/db:/DATA/htdocs/db nawa/torrentmonitor
 fi
